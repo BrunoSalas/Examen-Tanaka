73e057808c2ed39003db1866c27da7c68b20b693
jorgeluis15520 <103298795+jorgeluis15520@users.noreply.github.com>
2023-03-22 22:19:36 -0500

    while

--- Examen/Assets/Script/While.cs
+++ Examen/Assets/Script/While.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class While : MonoBehaviour
 {
+    int num = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        while (num < 10)
+        {
+            Debug.Log(num);
+            num++;
+        }
     }
 }
