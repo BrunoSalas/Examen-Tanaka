d3848c7512dbb3038ec01a961a07a3fd16eb29e0
andreajo119 <84331945+andreajo119@users.noreply.github.com>
2023-03-22 21:52:44 -0500

    If else

---
+++ Examen/Assets/Script/If else.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ifelse : MonoBehaviour
+{
+    public bool a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (a)
+        {
+            Debug.Log(a);
+        }
+        else
+        {
+            Debug.Log(a);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
